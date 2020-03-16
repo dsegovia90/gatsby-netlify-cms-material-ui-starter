622260de88a59d06ebdcc02226cfedcf8f5b8914
Daniel Segovia Arce <dsegovia@leftfieldlabs.com>
2020-03-16 16:55:59 -0700

    remove unused utils

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,7 +2,6 @@ const _ = require('lodash');
 const path = require('path');
 const { createFilePath } = require('gatsby-source-filesystem');
 const { fmImagesToRelative } = require('gatsby-remark-relative-images');
-const util = require('util');
 
 exports.createPages = ({ actions, graphql }) => {
   const { createPage } = actions;
